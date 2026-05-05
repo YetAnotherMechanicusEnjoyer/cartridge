e540522f94ddbb03688efd2231a2e5f01872b5f0
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-05-05 10:38:29 +0200

    [FEAT] add hitbox calculations based on collision map

---
+++ src/asm/hitbox.asm
@@ -0,0 +1,40 @@
+  .module hitbox
+  .area _CODE
+
+  .globl _get_hitbox_value
+  .globl _hitbox_x, _hitbox_y
+  .globl _collision_map
+
+  ; uint8_t get_hitbox_value(void)
+_get_hitbox_value:
+  ld a, (#_hitbox_y)
+  srl a
+  srl a
+  srl a
+  ld c, a
+  ld b, #0
+
+  ld l, c
+  ld h, b
+  add hl, hl
+  add hl, hl
+  ld e, l
+  ld d, h
+  add hl, hl
+  add hl, hl
+  add hl, de
+
+  ld a, (#_hitbox_x)
+  srl a
+  srl a
+  srl a
+  ld e, a
+  ld d, #0
+
+  add hl, de
+
+  ld de, #_collision_map
+  add hl, de
+  ld a, (hl)
+  ld e, a
+  ret
